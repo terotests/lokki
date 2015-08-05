a7efa749d01b1a81e51079470825f1d99904470e
Tero Tolonen <digikansio@gmail.com>
2015-08-05 15:25:50 +0300

    "commit message"

--- README.md
+++ README.md
@@ -1,1 +1,393 @@
-# lokki
+# Lokki lokinpoikanen
+
+A logging module for in-browser and node.js testing
+
+## Setup
+
+## Running
+
+```javascript
+// examples and docs coming later on.
+```
+
+# Licence
+
+MIT.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+ 
+
+
+   
+#### Class lokkiLoki
+
+
+
+
+
+   
+    
+##### trait _dataTrait
+
+- [guid](README.md#_dataTrait_guid)
+- [isArray](README.md#_dataTrait_isArray)
+- [isFunction](README.md#_dataTrait_isFunction)
+- [isObject](README.md#_dataTrait_isObject)
+
+
+    
+    
+    
+    
+
+
+   
+      
+    
+      
+            
+#### Class later
+
+
+- [add](README.md#later_add)
+- [after](README.md#later_after)
+- [asap](README.md#later_asap)
+- [every](README.md#later_every)
+- [once](README.md#later_once)
+- [onFrame](README.md#later_onFrame)
+- [polyfill](README.md#later_polyfill)
+- [removeFrameFn](README.md#later_removeFrameFn)
+
+
+
+   
+
+
+   
+
+
+
+      
+    
+
+
+
+
+
+   
+# Class lokkiLoki
+
+
+The class has following internal singleton variables:
+        
+        
+### lokkiLoki::constructor( options )
+
+```javascript
+
+```
+        
+
+
+   
+    
+## trait _dataTrait
+
+The class has following internal singleton variables:
+        
+* _eventOn
+        
+* _commands
+        
+        
+### <a name="_dataTrait_guid"></a>_dataTrait::guid(t)
+
+
+```javascript
+
+return Math.random().toString(36).substring(2, 15) +
+        Math.random().toString(36).substring(2, 15);
+
+```
+
+### <a name="_dataTrait_isArray"></a>_dataTrait::isArray(t)
+
+
+```javascript
+return Object.prototype.toString.call( t ) === '[object Array]';
+```
+
+### <a name="_dataTrait_isFunction"></a>_dataTrait::isFunction(fn)
+
+
+```javascript
+return Object.prototype.toString.call(fn) == '[object Function]';
+```
+
+### <a name="_dataTrait_isObject"></a>_dataTrait::isObject(t)
+
+
+```javascript
+return t === Object(t);
+```
+
+
+    
+    
+    
+    
+
+
+   
+      
+    
+      
+            
+# Class later
+
+
+The class has following internal singleton variables:
+        
+* _initDone
+        
+* _callers
+        
+* _oneTimers
+        
+* _everies
+        
+* _framers
+        
+        
+### <a name="later_add"></a>later::add(fn, thisObj, args)
+
+
+```javascript
+if(thisObj || args) {
+   var tArgs;
+   if( Object.prototype.toString.call( args ) === '[object Array]' ) {
+       tArgs = args;
+   } else {
+       tArgs = Array.prototype.slice.call(arguments, 2);
+       if(!tArgs) tArgs = [];
+   }
+   _callers.push([thisObj, fn, tArgs]);   
+} else {
+    _callers.push(fn);
+}
+```
+
+### <a name="later_after"></a>later::after(seconds, fn, name)
+
+
+```javascript
+
+if(!name) {
+    name = "time"+(new Date()).getTime()+Math.random(10000000);
+}
+
+_everies[name] = {
+    step : Math.floor(seconds * 1000),
+    fn : fn,
+    nextTime : 0,
+    remove : true
+};
+```
+
+### <a name="later_asap"></a>later::asap(fn)
+
+
+```javascript
+this.add(fn);
+
+```
+
+### <a name="later_every"></a>later::every(seconds, fn, name)
+
+
+```javascript
+
+if(!name) {
+    name = "time"+(new Date()).getTime()+Math.random(10000000);
+}
+
+_everies[name] = {
+    step : Math.floor(seconds * 1000),
+    fn : fn,
+    nextTime : 0
+};
+```
+
+### later::constructor( interval, fn )
+
+```javascript
+if(!_initDone) {
+    
+   this.polyfill();
+ 
+   var frame, cancelFrame;
+   if(typeof(window) != "undefined") {
+       var frame = window['requestAnimationFrame'], 
+           cancelFrame= window['cancelRequestAnimationFrame'];
+       ['', 'ms', 'moz', 'webkit', 'o'].forEach( function(x) { 
+           if(!frame) {
+            frame = window[x+'RequestAnimationFrame'];
+            cancelFrame = window[x+'CancelAnimationFrame'] 
+                                       || window[x+'CancelRequestAnimationFrame'];
+           }
+        });
+   }
+ 
+    if (!frame)
+        frame= function(cb) {
+            return setTimeout(cb, 16);
+        };
+ 
+    if (!cancelFrame)
+        cancelFrame = function(id) {
+            clearTimeout(id);
+        };    
+        
+    _callers = [];
+    _oneTimers = {};
+    _everies = {};
+    _framers = [];
+    var lastMs = 0;
+    
+    var _callQueQue = function() {
+       var ms = (new Date()).getTime();
+       var fn;
+       while(fn=_callers.shift()) {
+          if(Object.prototype.toString.call( fn ) === '[object Array]' ) {
+              fn[1].apply(fn[0], fn[2]);
+          } else {
+              fn();
+          }
+           
+       }
+       
+       for(var i=0; i<_framers.length;i++) {
+           var fFn = _framers[i];
+           fFn();
+       }
+       
+       for(var n in _oneTimers) {
+           if(_oneTimers.hasOwnProperty(n)) {
+               var v = _oneTimers[n];
+               v[0](v[1]);
+               delete _oneTimers[n];
+           }
+       }
+       
+       for(var n in _everies) {
+           if(_everies.hasOwnProperty(n)) {
+               var v = _everies[n];
+               if(v.nextTime < ms) {
+                   if(v.remove) {
+                       if(v.nextTime > 0) {
+                          v.fn(); 
+                          delete _everies[n];
+                       } else {
+                          v.nextTime = ms + v.step; 
+                       }
+                   } else {
+                       v.fn();
+                       v.nextTime = ms + v.step;
+                   }
+               }
+               if(v.until) {
+                   if(v.until < ms) {
+                       delete _everies[n];
+                   }
+               }
+           }
+       }       
+       
+       frame(_callQueQue);
+       lastMs = ms;
+    };
+    _callQueQue();
+    _initDone = true;
+}
+```
+        
+### <a name="later_once"></a>later::once(key, fn, value)
+
+
+```javascript
+// _oneTimers
+
+_oneTimers[key] = [fn,value];
+```
+
+### <a name="later_onFrame"></a>later::onFrame(fn)
+
+
+```javascript
+
+_framers.push(fn);
+```
+
+### <a name="later_polyfill"></a>later::polyfill(t)
+
+
+```javascript
+// --- let's not ---
+```
+
+### <a name="later_removeFrameFn"></a>later::removeFrameFn(fn)
+
+
+```javascript
+
+var i = _framers.indexOf(fn);
+if(i>=0) {
+    if(fn._onRemove) {
+        fn._onRemove();
+    }
+    _framers.splice(i,1);
+    return true;
+} else {
+    return false;
+}
+```
+
+
+
+   
+
+
+   
+
+
+
+      
+    
+
+
+
+
---
+++ release/lokki-0.01.js
@@ -0,0 +1,349 @@
+// The code template begins here
+'use strict';
+
+(function () {
+
+  var __amdDefs__ = {};
+
+  // The class definition is here...
+  var lokkiLoki_prototype = function lokkiLoki_prototype() {
+    // Then create the traits and subclasses for this class here...
+
+    // trait comes here...
+
+    (function (_myTrait_) {
+      var _eventOn;
+      var _commands;
+
+      // Initialize static variables here...
+
+      /**
+       * @param float t
+       */
+      _myTrait_.guid = function (t) {
+
+        return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+      };
+
+      /**
+       * @param float t
+       */
+      _myTrait_.isArray = function (t) {
+        return Object.prototype.toString.call(t) === '[object Array]';
+      };
+
+      /**
+       * @param float fn
+       */
+      _myTrait_.isFunction = function (fn) {
+        return Object.prototype.toString.call(fn) == '[object Function]';
+      };
+
+      /**
+       * @param float t
+       */
+      _myTrait_.isObject = function (t) {
+        return t === Object(t);
+      };
+    })(this);
+
+    // the subclass definition comes around here then
+
+    // The class definition is here...
+    var later_prototype = function later_prototype() {
+      // Then create the traits and subclasses for this class here...
+
+      (function (_myTrait_) {
+        var _initDone;
+        var _callers;
+        var _oneTimers;
+        var _everies;
+        var _framers;
+
+        // Initialize static variables here...
+
+        /**
+         * @param function fn
+         * @param float thisObj
+         * @param float args
+         */
+        _myTrait_.add = function (fn, thisObj, args) {
+          if (thisObj || args) {
+            var tArgs;
+            if (Object.prototype.toString.call(args) === '[object Array]') {
+              tArgs = args;
+            } else {
+              tArgs = Array.prototype.slice.call(arguments, 2);
+              if (!tArgs) tArgs = [];
+            }
+            _callers.push([thisObj, fn, tArgs]);
+          } else {
+            _callers.push(fn);
+          }
+        };
+
+        /**
+         * @param float seconds
+         * @param float fn
+         * @param float name
+         */
+        _myTrait_.after = function (seconds, fn, name) {
+
+          if (!name) {
+            name = 'time' + new Date().getTime() + Math.random(10000000);
+          }
+
+          _everies[name] = {
+            step: Math.floor(seconds * 1000),
+            fn: fn,
+            nextTime: 0,
+            remove: true
+          };
+        };
+
+        /**
+         * @param function fn
+         */
+        _myTrait_.asap = function (fn) {
+          this.add(fn);
+        };
+
+        /**
+         * @param float seconds
+         * @param float fn
+         * @param float name
+         */
+        _myTrait_.every = function (seconds, fn, name) {
+
+          if (!name) {
+            name = 'time' + new Date().getTime() + Math.random(10000000);
+          }
+
+          _everies[name] = {
+            step: Math.floor(seconds * 1000),
+            fn: fn,
+            nextTime: 0
+          };
+        };
+
+        if (_myTrait_.__traitInit && !_myTrait_.hasOwnProperty('__traitInit')) _myTrait_.__traitInit = _myTrait_.__traitInit.slice();
+        if (!_myTrait_.__traitInit) _myTrait_.__traitInit = [];
+        _myTrait_.__traitInit.push(function (interval, fn) {
+          if (!_initDone) {
+
+            this.polyfill();
+
+            var frame, cancelFrame;
+            if (typeof window != 'undefined') {
+              var frame = window['requestAnimationFrame'],
+                  cancelFrame = window['cancelRequestAnimationFrame'];
+              ['', 'ms', 'moz', 'webkit', 'o'].forEach(function (x) {
+                if (!frame) {
+                  frame = window[x + 'RequestAnimationFrame'];
+                  cancelFrame = window[x + 'CancelAnimationFrame'] || window[x + 'CancelRequestAnimationFrame'];
+                }
+              });
+            }
+
+            if (!frame) frame = function (cb) {
+              return setTimeout(cb, 16);
+            };
+
+            if (!cancelFrame) cancelFrame = function (id) {
+              clearTimeout(id);
+            };
+
+            _callers = [];
+            _oneTimers = {};
+            _everies = {};
+            _framers = [];
+            var lastMs = 0;
+
+            var _callQueQue = function _callQueQue() {
+              var ms = new Date().getTime();
+              var fn;
+              while (fn = _callers.shift()) {
+                if (Object.prototype.toString.call(fn) === '[object Array]') {
+                  fn[1].apply(fn[0], fn[2]);
+                } else {
+                  fn();
+                }
+              }
+
+              for (var i = 0; i < _framers.length; i++) {
+                var fFn = _framers[i];
+                fFn();
+              }
+
+              for (var n in _oneTimers) {
+                if (_oneTimers.hasOwnProperty(n)) {
+                  var v = _oneTimers[n];
+                  v[0](v[1]);
+                  delete _oneTimers[n];
+                }
+              }
+
+              for (var n in _everies) {
+                if (_everies.hasOwnProperty(n)) {
+                  var v = _everies[n];
+                  if (v.nextTime < ms) {
+                    if (v.remove) {
+                      if (v.nextTime > 0) {
+                        v.fn();
+                        delete _everies[n];
+                      } else {
+                        v.nextTime = ms + v.step;
+                      }
+                    } else {
+                      v.fn();
+                      v.nextTime = ms + v.step;
+                    }
+                  }
+                  if (v.until) {
+                    if (v.until < ms) {
+                      delete _everies[n];
+                    }
+                  }
+                }
+              }
+
+              frame(_callQueQue);
+              lastMs = ms;
+            };
+            _callQueQue();
+            _initDone = true;
+          }
+        });
+
+        /**
+         * @param  key
+         * @param float fn
+         * @param float value
+         */
+        _myTrait_.once = function (key, fn, value) {
+          // _oneTimers
+
+          _oneTimers[key] = [fn, value];
+        };
+
+        /**
+         * @param function fn
+         */
+        _myTrait_.onFrame = function (fn) {
+
+          _framers.push(fn);
+        };
+
+        /**
+         * @param float t
+         */
+        _myTrait_.polyfill = function (t) {};
+
+        /**
+         * @param float fn
+         */
+        _myTrait_.removeFrameFn = function (fn) {
+
+          var i = _framers.indexOf(fn);
+          if (i >= 0) {
+            if (fn._onRemove) {
+              fn._onRemove();
+            }
+            _framers.splice(i, 1);
+            return true;
+          } else {
+            return false;
+          }
+        };
+      })(this);
+    };
+
+    var later = function later(a, b, c, d, e, f, g, h) {
+      var m = this,
+          res;
+      if (m instanceof later) {
+        var args = [a, b, c, d, e, f, g, h];
+        if (m.__factoryClass) {
+          m.__factoryClass.forEach(function (initF) {
+            res = initF.apply(m, args);
+          });
+          if (typeof res == 'function') {
+            if (res._classInfo.name != later._classInfo.name) return new res(a, b, c, d, e, f, g, h);
+          } else {
+            if (res) return res;
+          }
+        }
+        if (m.__traitInit) {
+          m.__traitInit.forEach(function (initF) {
+            initF.apply(m, args);
+          });
+        } else {
+          if (typeof m.init == 'function') m.init.apply(m, args);
+        }
+      } else return new later(a, b, c, d, e, f, g, h);
+    };
+    // inheritance is here
+
+    later._classInfo = {
+      name: 'later'
+    };
+    later.prototype = new later_prototype();
+
+    (function (_myTrait_) {
+
+      // Initialize static variables here...
+
+      if (_myTrait_.__traitInit && !_myTrait_.hasOwnProperty('__traitInit')) _myTrait_.__traitInit = _myTrait_.__traitInit.slice();
+      if (!_myTrait_.__traitInit) _myTrait_.__traitInit = [];
+      _myTrait_.__traitInit.push(function (options) {});
+    })(this);
+  };
+
+  var lokkiLoki = function lokkiLoki(a, b, c, d, e, f, g, h) {
+    var m = this,
+        res;
+    if (m instanceof lokkiLoki) {
+      var args = [a, b, c, d, e, f, g, h];
+      if (m.__factoryClass) {
+        m.__factoryClass.forEach(function (initF) {
+          res = initF.apply(m, args);
+        });
+        if (typeof res == 'function') {
+          if (res._classInfo.name != lokkiLoki._classInfo.name) return new res(a, b, c, d, e, f, g, h);
+        } else {
+          if (res) return res;
+        }
+      }
+      if (m.__traitInit) {
+        m.__traitInit.forEach(function (initF) {
+          initF.apply(m, args);
+        });
+      } else {
+        if (typeof m.init == 'function') m.init.apply(m, args);
+      }
+    } else return new lokkiLoki(a, b, c, d, e, f, g, h);
+  };
+  // inheritance is here
+
+  lokkiLoki._classInfo = {
+    name: 'lokkiLoki'
+  };
+  lokkiLoki.prototype = new lokkiLoki_prototype();
+
+  (function () {
+    if (typeof define !== 'undefined' && define !== null && define.amd != null) {
+      __amdDefs__['lokkiLoki'] = lokkiLoki;
+      this.lokkiLoki = lokkiLoki;
+    } else if (typeof module !== 'undefined' && module !== null && module.exports != null) {
+      module.exports['lokkiLoki'] = lokkiLoki;
+    } else {
+      this.lokkiLoki = lokkiLoki;
+    }
+  }).call(new Function('return this')());
+
+  if (typeof define !== 'undefined' && define !== null && define.amd != null) {
+    define(__amdDefs__);
+  }
+}).call(new Function('return this')());
+
+// --- let's not ---
